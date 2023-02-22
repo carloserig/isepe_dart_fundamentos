126fb0d8e092332609e641083cc13d2cc8186262
carloserig <carlos.erig@gmail.com>
2023-02-21 22:47:05 -0300

    2 commit

--- lib/variaveis/1_variaveis.dart
+++ lib/variaveis/1_variaveis.dart
@@ -1,7 +1,19 @@
 void main() {
   
+  //int
+  //int numero = 10;
+  
+  //int numero;
+  //numero = 10;
+
+  int numero = 10;
+
+   num n1 = 10.1;
+
+  print('O número é $n1');
+
   //bool  
   bool isCasado = true;
-  print('É casado? ${isCasado ? 'Sim' : 'Não'}');
+  //print('É casado? ${isCasado ? 'Sim' : 'Não'}');
 
 }
