6f632a9bcd0f5f362568b0d2799531adc9cd56f8
carloserig <carlos.erig@gmail.com>
2023-03-13 23:11:57 -0300

    Alterações 12_03_23

--- lib/conversoes_manipulacoes.dart/1_manipulacao_strings.dart
+++ lib/conversoes_manipulacoes.dart/1_manipulacao_strings.dart
@@ -1,3 +1,20 @@
 void main() {
+  final nome = 'Carlos Alberto Erig';
+
+  var substring = nome.substring(7);
+  var substring2 = nome.substring(0,7);
+  var substring3 = nome.toUpperCase();
   
+  var paciente = 'Carlos Erig|40|Programador|PR';
+  //var nomeCliente = paciente.substring(0,11);
+  //var idadeCliente = paciente.substring(12,14);
+  //print('Nome $nomeCliente, idade $idadeCliente');
+  var dadosPaciente = paciente.split('|');
+  print(dadosPaciente);
+  //print(dadosPaciente[0]);
+  //print(dadosPaciente[1]);
+  
+  for(var dado in dadosPaciente) {
+    print(dado);
+  }
 }
