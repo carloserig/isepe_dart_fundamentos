59bfe66dbb9db63bb883fcb6f873b313f017eb4d
carloserig <carlos.erig@gmail.com>
2023-03-13 23:13:26 -0300

    Alterações 13_03_23

--- lib/conversoes_manipulacoes.dart/2_manipulacao_numeros.dart
+++ lib/conversoes_manipulacoes.dart/2_manipulacao_numeros.dart
@@ -1,3 +1,18 @@
 void main() {
+  final valorDouble = 10.65;
+  print(valorDouble.round());
+  print(valorDouble.roundToDouble());
   
+  final valorSt = '50';
+  
+  final valorIn = int.parse(valorSt);
+  print(valorIn);
+
+  final valorStr = '50a';
+
+  final vlrInt = int.tryParse(valorStr);
+  print(vlrInt ?? 'valor nulo');
+
+  final preco = 40.45368;
+  print(preco.toStringAsFixed(2));
 }
